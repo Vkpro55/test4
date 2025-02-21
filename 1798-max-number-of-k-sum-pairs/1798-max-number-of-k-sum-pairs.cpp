66ae1367761ad4b2c1d12b729e26e6c3be5994a4
Vinod Kumar <83464767+Vkpro55@users.noreply.github.com>
2025-02-21 11:26:35 +0530

    Time: 202 ms (5.03%), Space: 91.6 MB (5.06%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
@@ -2,30 +2,37 @@ class Solution {
 public:
     int maxOperations(vector<int> &nums, int k)
 {
-    unordered_map<int, int> store; /* === ele, freq ===*/
-    int count = 0;
-
+    unordered_map<int, int> store, visited; /* ==== ele, freq ==== */
     for (int i = 0; i < nums.size(); i++)
     {
-        /* === Needed Value===*/
-        int val = k - nums[i];
-        if (store.find(val) != store.end())
+        store[nums[i]] = store[nums[i]] + 1;
+    }
+
+    int count = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int number = nums[i];
+        int partner = k - nums[i];
+
+        /* To ensure that I can't visited the same element again in different pair*/
+        if (visited[number] == 0 && visited[partner] == 0)
         {
-            cout << i << endl;
-
-            count = count + 1;
-            store[val]--;
-
-            if (store[val] <= 0)
+            /* ====  1 2 3 4 3 2 3 3  k=6 ====*/
+            if (number == partner)
             {
-                store.erase(val);
+                /* 3-> 4 */
+                count += store[number] / 2;
+            }
+            else
+            {
+                count += min(store[number], store[partner]);
             }
         }
-        else
-        {
-            store[nums[i]] = store[nums[i]] + 1;
-        }
+
+        visited[number] = 1;
+        visited[partner] = 1;
     }
+
     return count;
 }
 };
